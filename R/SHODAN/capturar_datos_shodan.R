8905b68bf92456b51c2aa9fb81077e8c5979664b
Alejandro Moreno <amperis@gmail.com>
2018-05-14 19:01:18 +0200

    no message

--- R/SHODAN/capturar_datos_shodan.R
+++ R/SHODAN/capturar_datos_shodan.R
@@ -28,24 +28,30 @@ account_profile()
 csv <- read.csv(file="./FUENTESDATOS/dispositivos_industriales.csv", head=TRUE, sep=";")
 
 result<-data.frame()
+
 #--- recorrecmos cada fila del CSV
 for (i in 1:nrow(csv)) {
+  
   #--- leyendo datos del CSV
   fabricante<-as.character(csv[i,"fabricante"])
   producto<-as.character(csv[i,"producto"])
   cadena_busqueda<-as.character(csv[i,"cadena_busqueda"])
+  
   #--- realizamos la busqueda dentro de Shodan
   print(cadena_busqueda)
   aux<-shodan_search(cadena_busqueda)
   print(aux$total)
   Sys.sleep(1)
+  
   #--- si encontramos algo en Shodan
   if (aux$total > 0) {
      v_fabricante<-rstring(fabricante,length(aux$matches$ip_str))
      v_producto<-rstring(producto,length(aux$matches$ip_str))
      v_cadena_busqueda<-rstring(cadena_busqueda,length(aux$matches$ip_str))
+     
      #--- construyo una tabla con los datos obtenidos
      result_cvs<-data.frame(v_fabricante, v_producto, v_cadena_busqueda, aux$matches$ip_str, aux$matches$isp, aux$matches$location$city, aux$matches$location$longitude, aux$matches$location$latitude, aux$matches$location$country_name, aux$matches$location$country_code3, aux$matches$os, aux$matches$org, aux$matches$asn )
+     
      #--- adjunto los datos obtenidos a los anteriores
      result <- rbind(result,result_cvs)
   }
@@ -53,6 +59,7 @@ for (i in 1:nrow(csv)) {
 
 #--- guardo todos mis datos en un CSV
 write.table(result,file="./FUENTESDATOS/resultado_busqueda_shodan.csv",quote=FALSE, sep=";", row.names = FALSE)
+
 
 
 
